b02bd74299c9e8779fc17310706db4097cea375e
taylulz <taylulzcode@gmail.com>
2020-11-02 15:43:44 -0800

    save progress on address-book for the day
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,22 +17,26 @@ AddressBook.prototype.assignId = function() {
 }
 
 AddressBook.prototype.findContact = function(id) {
-  for (let i=0; i<this.contacts.length; i++) {
-    if (this.contacts[i].id == id) {
-      return this.contacts[i];
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
     }
   };
   return false;
 }
 
 AddressBook.prototype.deleteContact = function(id) {
-  for (let i = 0; i < this.contacts.length; i++) {
-    if (this.contacts[i].id == id) {
-      delete this.contacts[i];
-      return true;
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
     }
   };
-  return false
+  return false;
 }
 
 // Business Logic for Contacts
@@ -51,8 +55,8 @@ let addressBook = new AddressBook();
 $(document).ready(function() {
   $("#contactInfo").submit(function(e) {
     e.preventDefault();
-    $("#contactList").text("");
-
+    
+    
     let firstName = $("input#firstName").val();
     let lastName = $("input#lastName").val();
     let phoneNumber = $("input#phoneNumber").val();
@@ -60,13 +64,35 @@ $(document).ready(function() {
     addressBook.addContact(contact1)
     console.log(addressBook);
 
-    for (let i = 0; i < addressBook.contacts.length; i++) {   
-    $("#contactList").append("<li>" + addressBook.contacts[i].fullName() + "</li>")
-    }
-
-  });
-      $("#deleteButton").click(function(e) {
-        e.preventDefault();
-
+    // function displayContacts () {
+      $("#contactList").html("");
+      let htmlForContactInfo = "";
+      addressBook.contacts.forEach(function(contact) {
+        htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
       });
+      $("#contactList").html(htmlForContactInfo);
+    // };
+
+    function removeContact(){
+      $("li").click(function() {
+        console.log(this.id);
+        addressBook.deleteContact(this.id);
+        let htmlForContactInfo = "";
+        addressBook.contacts.forEach(function(contact) {
+          htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+        });
+        $("#contactList").html(htmlForContactInfo);
+      });
+      removeContact();
+    }
+    removeContact();
+
+  }); 
+
 });
+
+
+// 1. click the contact
+// 2. find that contact
+// 3. then delete that contact
+// addressBook.d
