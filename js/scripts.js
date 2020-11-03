d27feeecb66e4f7fafb888f2b5eae7ca238ebe1d
taylulz <taylulzcode@gmail.com>
2020-11-03 12:00:43 -0800

    save progress for lunch
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -47,3 +47,50 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+// User Interface Logic ----------
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners(){
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+
+$(document).ready(function() {
+  attachContactListeners();
+    $("form#new-contact").submit(function(e) {
+      e.preventDefault();
+      const inputtedFirstName = $("input#new-first-name").val();
+      const inputtedLastName = $("input#new-last-name").val();
+      const inputtedPhoneNumber = $("input#new-phone-number").val();
+      let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+      addressBook.addContact(newContact);
+      displayContactDetails(addressBook);
+  });
+});
